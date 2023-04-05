e1e6909734d064c595906e46c223d7743c1130e8
Shenith <shenithsandeepa@gmail.com>
2023-04-05 13:48:05 +0530

    Create MAProgressCircle molecule

---
+++ src/components/molecules/MAProgressCircle/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import MAText from '../../atoms/MAText.tsx';
+import theme from '../../../utils/theme';
+import {styles} from './styles';
+
+interface Props {
+  fill: number;
+}
+
+const MAProgressCircle = ({fill}: Props) => {
+  return (
+    <AnimatedCircularProgress
+      size={50}
+      width={5}
+      fill={fill}
+      tintColor={theme.colors.success}
+      padding={2}
+      rotation={0}
+      style={styles.containerStyle}
+      backgroundColor={theme.colors.grey}>
+      {() => (
+        <MAText type={'body3'} color={theme.colors.white}>
+          {`${fill}%`}
+        </MAText>
+      )}
+    </AnimatedCircularProgress>
+  );
+};
+
+export default MAProgressCircle;
---
+++ src/components/molecules/MAProgressCircle/styles.ts
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+import theme from '../../../utils/theme';
+
+export const styles = StyleSheet.create({
+  containerStyle: {backgroundColor: theme.colors.black, borderRadius: 100},
+});
