4fb7bd435e631373d85e740ea6bd799dd5dcafd5
Shenith <shenithsandeepa@gmail.com>
2023-04-07 17:37:56 +0530

    Remove API_KEY from config file

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,2 @@
 export const API_BASE_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '9618b5cf6ae9661f92fff553c697bed4';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/w300/';
